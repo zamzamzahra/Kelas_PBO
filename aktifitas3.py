03de469985a2c37ca2b76fea042335b285516c1a
zamzamzahra <zamzamkhzzz@gmail.com>
2021-02-26 09:55:08 +0700

    Aktifitas 3

---
+++ aktifitas3.py
@@ -0,0 +1,14 @@
+#PERCABANGAN
+print("Percabangan pada Bahasa Pemograman Python")
+print("Pilih Bahasa Pemograman: ")
+print("1. Python")
+print("2. C")
+
+pilih=int(input("Pilih Menu (1/2) : "))
+
+if pilih == 1:
+    print("Bahasa Pemograman yang dipilih adalah Python")
+elif pilih == 2:
+    print("Bahasa Pemograman yang dipilih adalah C")
+else :
+    print("Pilihan Anda tidak tersedia!!!")
