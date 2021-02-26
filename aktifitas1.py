a6b466cdc554c468277f4ffa25cef2fad78ebb2d
zamzamzahra <zamzamkhzzz@gmail.com>
2021-02-26 09:22:28 +0700

    Zamzam Kholidatuzzahra Aktifitas 1

---
+++ aktifitas1.py
@@ -0,0 +1,10 @@
+#INPUT
+nama=input("Masukkan nama Anda :")
+umur=19
+
+#OUTPUT
+print("")
+print("====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PHYTHON====")
+print("nama Anda :", nama)
+print("nama Anda :", umur)
+print("")
